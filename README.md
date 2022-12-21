6c022dbf37cc1e6739b27e0590784e0b84be0320
Prescott Murphy <prurph@gmail.com>
2022-12-21 14:35:09 -0500

    Configure Tailwind for default styling and add Vim cheatsheet

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Cheatsheets
 
 Serve some random cheatsheets so I can show them in a fancy macOS browser popup.
+
+## Development
+
+Run `pnpm next`
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>📃 Cheatsheets</span>,
+  project: {
+    link: "https://github.com/prurph/cheatsheets",
+  },
+  docsRepositoryBase: "https://github.com/prurph/cheatsheets"
+}
